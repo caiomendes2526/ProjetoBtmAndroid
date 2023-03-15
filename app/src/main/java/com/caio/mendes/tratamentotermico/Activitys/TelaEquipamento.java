03eb0b20236d113538cfa611152b49f6525d1445
Caio Mendes <36896578+caiomendes2526@users.noreply.github.com>
2023-03-15 17:18:47 -0300

    zzzzzzzzz

--- app/src/main/java/com/caio/mendes/tratamentotermico/Activitys/TelaEquipamento.java
+++ app/src/main/java/com/caio/mendes/tratamentotermico/Activitys/TelaEquipamento.java
@@ -1,8 +1,15 @@
 package com.caio.mendes.tratamentotermico.Activitys;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.caio.mendes.tratamentotermico.Activitys.TelaLogin.Nf_alta;
+import static com.caio.mendes.tratamentotermico.Activitys.TelaLogin.Nf_baixa;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +18,8 @@ import com.caio.mendes.tratamentotermico.ListarDados.ListarAdapterEquipamento;
 import com.caio.mendes.tratamentotermico.ListarDados.ListarEquipamento;
 import com.caio.mendes.tratamentotermico.ModuloConexao.ModuloConexao;
 import com.caio.mendes.tratamentotermico.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
@@ -23,12 +32,14 @@ public class TelaEquipamento extends AppCompatActivity {
 
     ModuloConexao moduloConexao = new ModuloConexao();
 
+    private String condicao = "0";
 
     ListView ListViewEquipamento;
 
     ListarAdapterEquipamento listarAdapterEquipamento;
 
     List<ListarEquipamento> Lista2;
+    FloatingActionButton fab_add_equipamento;
 
     public static String Equipamento;
 
@@ -37,6 +48,8 @@ public class TelaEquipamento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_equipamento);
 
+        fab_add_equipamento = (FloatingActionButton) findViewById(R.id.fab_add_equipamento);
+
         ListViewEquipamento = (ListView) findViewById(R.id.ListViewEquipamento);
 
         Lista2 = new ArrayList<ListarEquipamento>();
@@ -44,6 +57,28 @@ public class TelaEquipamento extends AppCompatActivity {
         listarAdapterEquipamento = new ListarAdapterEquipamento(TelaEquipamento.this, Lista2);
 
         ListViewEquipamento.setAdapter(listarAdapterEquipamento);
+
+        System.out.println("Nf alta " + Nf_alta );
+
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String myValue = prefs.getString("myKey", condicao);
+        condicao = myValue;
+
+        System.out.println("Minha Váriavel: " + condicao);
+
+        if (condicao.equals("1")) {
+
+            fab_add_equipamento.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+            fab_add_equipamento.setImageResource(R.drawable.alarmeativado);
+         //   btnAtivadoDesativado.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.alarmeativado,0,0);
+        //    btnAtivadoDesativado.setText("ALARMES ATIVADOS");
+        }
+        if (condicao.equals("0")) {
+            fab_add_equipamento.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white)));
+            fab_add_equipamento.setImageResource(R.drawable.alarmedesativado);
+       //     btnAtivadoDesativado.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.alarmedesativado,0,0);
+       //     btnAtivadoDesativado.setText("ALARMES DESATIVADOS");
+        }
 
 
         // metodo para setar itens de uma lista
@@ -63,6 +98,54 @@ public class TelaEquipamento extends AppCompatActivity {
         });
 
         ListarEquipamento();
+
+
+        fab_add_equipamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+
+            }
+        });
+
+        fab_add_equipamento.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                if (Nf_alta != null) {
+
+                    if (condicao.equals("1")) {
+                        condicao = "0";
+                        gravarCondicao();
+                        FirebaseMessaging.getInstance().unsubscribeFromTopic(Nf_alta);
+                        FirebaseMessaging.getInstance().unsubscribeFromTopic(Nf_baixa);
+
+                        fab_add_equipamento.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.white)));
+                        fab_add_equipamento.setImageResource(R.drawable.alarmedesativado);
+
+                    } else if (condicao.equals("0")) {
+                        condicao = "1";
+                        gravarCondicao();
+                        FirebaseMessaging.getInstance().subscribeToTopic(Nf_alta);
+                        FirebaseMessaging.getInstance().subscribeToTopic(Nf_baixa);
+                        fab_add_equipamento.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.black)));
+                        fab_add_equipamento.setImageResource(R.drawable.alarmeativado);
+
+                    }
+
+
+                }
+
+
+
+                return false;
+            }
+        });
+
+
 
     }
 
@@ -99,6 +182,23 @@ public class TelaEquipamento extends AppCompatActivity {
 
         }
     }
+
+
+    private void gravarCondicao() {
+
+        try {
+            SharedPreferences prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("myKey", condicao);
+            editor.apply();
+
+
+        } catch (Exception e) {
+
+        }
+    }
+
+
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/caio/mendes/tratamentotermico/Activitys/TelaPrincipal.java
+++ app/src/main/java/com/caio/mendes/tratamentotermico/Activitys/TelaPrincipal.java
@@ -20,7 +20,6 @@ public class TelaPrincipal extends AppCompatActivity {
     private Button btnTemperatura;
     private Button btnAlarme;
     private Button btnAtivadoDesativado;
-    private String condicao = "0";
 
 
     @Override
@@ -33,23 +32,7 @@ public class TelaPrincipal extends AppCompatActivity {
         btnAlarme = (Button) findViewById(R.id.btnAlarme);
         btnAtivadoDesativado = (Button) findViewById(R.id.btnAtivaDessativa);
 
-        System.out.println("Nf alta " + Nf_alta );
 
-        SharedPreferences prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        String myValue = prefs.getString("myKey", condicao);
-        condicao = myValue;
-
-        System.out.println("Minha Váriavel: " + condicao);
-
-        if (condicao.equals("1")) {
-
-            btnAtivadoDesativado.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.alarmeativado,0,0);
-            btnAtivadoDesativado.setText("ALARMES ATIVADOS");
-        }
-        if (condicao.equals("0")) {
-            btnAtivadoDesativado.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.alarmedesativado,0,0);
-            btnAtivadoDesativado.setText("ALARMES DESATIVADOS");
-        }
 
 
         btnTemperatura.setOnClickListener(new View.OnClickListener() {
@@ -74,43 +57,14 @@ public class TelaPrincipal extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (Nf_alta != null) {
 
-                    if (condicao.equals("1")) {
-                        condicao = "0";
-                        gravarCondicao();
-                        FirebaseMessaging.getInstance().unsubscribeFromTopic(Nf_alta);
-                        FirebaseMessaging.getInstance().unsubscribeFromTopic(Nf_baixa);
-                        finish();
-                    } else if (condicao.equals("0")) {
-                        condicao = "1";
-                        gravarCondicao();
-                        FirebaseMessaging.getInstance().subscribeToTopic(Nf_alta);
-                        FirebaseMessaging.getInstance().subscribeToTopic(Nf_baixa);
-                        finish();
-                    }
-
-
-                }
 
             }
         });
 
     }
 
-    private void gravarCondicao() {
 
-        try {
-            SharedPreferences prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString("myKey", condicao);
-            editor.apply();
-
-
-        } catch (Exception e) {
-
-        }
-    }
 
 
 }
